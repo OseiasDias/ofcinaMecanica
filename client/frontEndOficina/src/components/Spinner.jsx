7511df841fcfe1d297e028adabffe1b03b70d49f
Oseias Dias <oseiasdias1953@gmail.com>
2024-10-10 17:32:19 +0100

    utlimas mudancas 17:32

--- client/frontEndOficina/src/components/Spinner.jsx
+++ client/frontEndOficina/src/components/Spinner.jsx
@@ -1,31 +1,27 @@
 // Spinner.js
-import '../css/spinner.css'; // Adicionar estilos
-
-
+import "../css/spinner.css"; // Adicionar estilos
 
 const SpinnerFill = () => {
   return (
-    <div className="spinner-container">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="300"
-        height="300"
-        viewBox="0 0 100 100"
-        className="gear-spinner"
-      >
-        <g transform="translate(50,50)">
-          <g transform="translate(-50,-50)">
-            <path
-              fill="#ddd"
-              d="M50 30a20 20 0 1 0 0 40 20 20 0 1 0 0-40zm-10 35v-2.5h3.9l1.2-3-3-2.2v-2.5h3.9l1.2-3-3-2.2v-2.5h3.9l1.2-3-3-2.2v-2.5h6v2.5l-3 2.2 1.2 3h3.9v2.5l-3 2.2 1.2 3h3.9v2.5l-3 2.2 1.2 3h-6v-2.5l3-2.2-1.2-3H40z"
-            />
+    <div className="container-fluid">
+      <div className="spinner-container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="250"
+          height="250"
+          viewBox="0 0 100 100"
+          className="gear-spinner"
+        >
+          <g transform="translate(50,50)">
+            <g transform="translate(-50,-50)">
+              <path
+                fill="#ddd"
+                d="M50 30a20 20 0 1 0 0 40 20 20 0 1 0 0-40zm-10 35v-2.5h3.9l1.2-3-3-2.2v-2.5h3.9l1.2-3-3-2.2v-2.5h3.9l1.2-3-3-2.2v-2.5h6v2.5l-3 2.2 1.2 3h3.9v2.5l-3 2.2 1.2 3h3.9v2.5l-3 2.2 1.2 3h-6v-2.5l3-2.2-1.2-3H40z"
+              />
+            </g>
           </g>
-        </g>
-      </svg>
-      
-      
-     
-     
+        </svg>
+      </div>
     </div>
   );
 };
